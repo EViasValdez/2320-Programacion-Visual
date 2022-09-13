b073974d94cd791ee34c15640ad4fa11e1f28b92
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2022-09-13 11:32:02 -0500

    Add files via upload

--- Ejercicios/Ejercicios/Form1.Designer.cs
+++ Ejercicios/Ejercicios/Form1.Designer.cs
@@ -42,9 +42,10 @@
             this.label1.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(192, 24);
+            this.label1.Location = new System.Drawing.Point(249, 41);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(205, 47);
+            this.label1.Size = new System.Drawing.Size(269, 59);
             this.label1.TabIndex = 0;
             this.label1.Text = "Grupo 2320 - IS";
             // 
@@ -53,9 +54,10 @@
             this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(187, 136);
+            this.button1.Location = new System.Drawing.Point(216, 155);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(210, 75);
+            this.button1.Size = new System.Drawing.Size(332, 87);
             this.button1.TabIndex = 1;
             this.button1.Text = "Haga click aqui";
             this.button1.UseVisualStyleBackColor = false;
@@ -64,9 +66,10 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(205, 393);
+            this.button2.Location = new System.Drawing.Point(261, 473);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(183, 82);
+            this.button2.Size = new System.Drawing.Size(244, 101);
             this.button2.TabIndex = 2;
             this.button2.Text = "Todavia no presiones esto";
             this.button2.UseVisualStyleBackColor = true;
@@ -77,25 +80,28 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Maroon;
-            this.label2.Location = new System.Drawing.Point(165, 276);
+            this.label2.Location = new System.Drawing.Point(221, 340);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(270, 37);
+            this.label2.Size = new System.Drawing.Size(340, 47);
             this.label2.TabIndex = 3;
             this.label2.Text = "DESACTIVADO";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(603, 504);
+            this.ClientSize = new System.Drawing.Size(779, 620);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Ejercicios";
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
--- Practica/Practica/form.Designer.cs
+++ Practica/Practica/form.Designer.cs
@@ -39,9 +39,10 @@
             this.button1.Font = new System.Drawing.Font("Gadugi", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
-            this.button1.Location = new System.Drawing.Point(295, 215);
+            this.button1.Location = new System.Drawing.Point(393, 265);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(211, 79);
+            this.button1.Size = new System.Drawing.Size(281, 97);
             this.button1.TabIndex = 0;
             this.button1.Text = "Presione aquí";
             this.button1.UseVisualStyleBackColor = true;
@@ -53,23 +54,25 @@
             this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label1.Font = new System.Drawing.Font("Ebrima", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Yellow;
-            this.label1.Location = new System.Drawing.Point(286, 27);
+            this.label1.Location = new System.Drawing.Point(391, 116);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(233, 50);
+            this.label1.Size = new System.Drawing.Size(293, 62);
             this.label1.TabIndex = 1;
             this.label1.Text = "GRUPO 2320";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(810, 614);
+            this.ClientSize = new System.Drawing.Size(1080, 756);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Grupo 2320";
             this.ResumeLayout(false);
             this.PerformLayout();
 
