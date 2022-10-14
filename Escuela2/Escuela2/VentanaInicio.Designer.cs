92b624098858f5d2c2fff430280b9d770cda60b3
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2022-10-14 11:24:22 -0500

    Add file via upload

--- Escuela2/Escuela2/VentanaInicio.Designer.cs
+++ Escuela2/Escuela2/VentanaInicio.Designer.cs
@@ -41,10 +41,11 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox1.Image = global::Escuela2.Properties.Resources.foto_edi_1_4_;
             this.pictureBox1.Location = new System.Drawing.Point(16, 15);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(1034, 243);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -77,7 +78,7 @@
             // 
             this.tbxusuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tbxusuario.Location = new System.Drawing.Point(533, 342);
-            this.tbxusuario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbxusuario.Margin = new System.Windows.Forms.Padding(4);
             this.tbxusuario.Name = "tbxusuario";
             this.tbxusuario.Size = new System.Drawing.Size(400, 30);
             this.tbxusuario.TabIndex = 3;
@@ -86,7 +87,7 @@
             // 
             this.txbcontraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txbcontraseña.Location = new System.Drawing.Point(533, 400);
-            this.txbcontraseña.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txbcontraseña.Margin = new System.Windows.Forms.Padding(4);
             this.txbcontraseña.Name = "txbcontraseña";
             this.txbcontraseña.Size = new System.Drawing.Size(400, 30);
             this.txbcontraseña.TabIndex = 4;
@@ -95,7 +96,7 @@
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(905, 495);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(129, 44);
             this.button1.TabIndex = 5;
@@ -116,13 +117,14 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "VentanaInicio";
             this.Text = "Universidad Politecnica De Tecamac";
             this.Load += new System.EventHandler(this.Inicio_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
--- Escuela2/Escuela2/VentanaMateria.Designer.cs
+++ Escuela2/Escuela2/VentanaMateria.Designer.cs
@@ -62,9 +62,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(190, 40);
+            this.label1.Location = new System.Drawing.Point(253, 49);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(496, 29);
+            this.label1.Size = new System.Drawing.Size(622, 36);
             this.label1.TabIndex = 0;
             this.label1.Text = "Universidad Politecnica De Tecamac";
             // 
@@ -84,18 +85,21 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(38, 111);
+            this.groupBox1.Location = new System.Drawing.Point(51, 137);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(805, 361);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(1073, 444);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Registro de la materia";
             // 
             // btonsiguiente
             // 
-            this.btonsiguiente.Location = new System.Drawing.Point(655, 270);
+            this.btonsiguiente.Location = new System.Drawing.Point(873, 332);
+            this.btonsiguiente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btonsiguiente.Name = "btonsiguiente";
-            this.btonsiguiente.Size = new System.Drawing.Size(107, 62);
+            this.btonsiguiente.Size = new System.Drawing.Size(143, 76);
             this.btonsiguiente.TabIndex = 12;
             this.btonsiguiente.Text = "Siguiente ventana";
             this.btonsiguiente.UseVisualStyleBackColor = true;
@@ -103,9 +107,10 @@
             // 
             // btondatos
             // 
-            this.btondatos.Location = new System.Drawing.Point(528, 270);
+            this.btondatos.Location = new System.Drawing.Point(704, 332);
+            this.btondatos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btondatos.Name = "btondatos";
-            this.btondatos.Size = new System.Drawing.Size(107, 62);
+            this.btondatos.Size = new System.Drawing.Size(143, 76);
             this.btondatos.TabIndex = 11;
             this.btondatos.Text = "Mostrar datos en la tabla";
             this.btondatos.UseVisualStyleBackColor = true;
@@ -113,11 +118,13 @@
             // 
             // btnregistrar
             // 
+            this.btnregistrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.btnregistrar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnregistrar.Image = global::Escuela2.Properties.Resources.registrarse;
-            this.btnregistrar.Location = new System.Drawing.Point(586, 194);
+            this.btnregistrar.Location = new System.Drawing.Point(781, 239);
+            this.btnregistrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnregistrar.Name = "btnregistrar";
-            this.btnregistrar.Size = new System.Drawing.Size(118, 70);
+            this.btnregistrar.Size = new System.Drawing.Size(157, 86);
             this.btnregistrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.btnregistrar.TabIndex = 10;
             this.btnregistrar.TabStop = false;
@@ -131,37 +138,48 @@
             this.Materia,
             this.Horas,
             this.Creditos});
-            this.dgvinfo.Location = new System.Drawing.Point(33, 184);
+            this.dgvinfo.Location = new System.Drawing.Point(44, 226);
+            this.dgvinfo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgvinfo.Name = "dgvinfo";
-            this.dgvinfo.Size = new System.Drawing.Size(443, 150);
+            this.dgvinfo.RowHeadersWidth = 51;
+            this.dgvinfo.Size = new System.Drawing.Size(591, 185);
             this.dgvinfo.TabIndex = 9;
             // 
             // Id_materia
             // 
             this.Id_materia.HeaderText = "Id_materia";
+            this.Id_materia.MinimumWidth = 6;
             this.Id_materia.Name = "Id_materia";
+            this.Id_materia.Width = 125;
             // 
             // Materia
             // 
             this.Materia.HeaderText = "Materia";
+            this.Materia.MinimumWidth = 6;
             this.Materia.Name = "Materia";
+            this.Materia.Width = 125;
             // 
             // Horas
             // 
             this.Horas.HeaderText = "Horas";
+            this.Horas.MinimumWidth = 6;
             this.Horas.Name = "Horas";
+            this.Horas.Width = 125;
             // 
             // Creditos
             // 
             this.Creditos.HeaderText = "Creditos";
+            this.Creditos.MinimumWidth = 6;
             this.Creditos.Name = "Creditos";
+            this.Creditos.Width = 125;
             // 
             // textBox3
             // 
             this.textBox3.Enabled = false;
-            this.textBox3.Location = new System.Drawing.Point(188, 137);
+            this.textBox3.Location = new System.Drawing.Point(251, 169);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(291, 22);
+            this.textBox3.Size = new System.Drawing.Size(387, 26);
             this.textBox3.TabIndex = 8;
             // 
             // cbcreditos
@@ -178,26 +196,29 @@
             "8",
             "9",
             "10"});
-            this.cbcreditos.Location = new System.Drawing.Point(497, 83);
+            this.cbcreditos.Location = new System.Drawing.Point(663, 102);
+            this.cbcreditos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbcreditos.Name = "cbcreditos";
-            this.cbcreditos.Size = new System.Drawing.Size(39, 24);
+            this.cbcreditos.Size = new System.Drawing.Size(51, 28);
             this.cbcreditos.TabIndex = 7;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(426, 86);
+            this.label5.Location = new System.Drawing.Point(568, 106);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(58, 16);
+            this.label5.Size = new System.Drawing.Size(72, 20);
             this.label5.TabIndex = 6;
             this.label5.Text = "Creditos";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(426, 45);
+            this.label4.Location = new System.Drawing.Point(568, 55);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 16);
+            this.label4.Size = new System.Drawing.Size(65, 20);
             this.label4.TabIndex = 5;
             this.label4.Text = "Materia";
             // 
@@ -211,74 +232,84 @@
             "3",
             "2",
             "1"});
-            this.cbhoras.Location = new System.Drawing.Point(244, 86);
+            this.cbhoras.Location = new System.Drawing.Point(325, 106);
+            this.cbhoras.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbhoras.Name = "cbhoras";
-            this.cbhoras.Size = new System.Drawing.Size(40, 24);
+            this.cbhoras.Size = new System.Drawing.Size(52, 28);
             this.cbhoras.TabIndex = 4;
             // 
             // txbmateria
             // 
-            this.txbmateria.Location = new System.Drawing.Point(497, 42);
+            this.txbmateria.Location = new System.Drawing.Point(663, 52);
+            this.txbmateria.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txbmateria.Name = "txbmateria";
-            this.txbmateria.Size = new System.Drawing.Size(100, 22);
+            this.txbmateria.Size = new System.Drawing.Size(132, 26);
             this.txbmateria.TabIndex = 3;
             // 
             // txbxidmateria
             // 
-            this.txbxidmateria.Location = new System.Drawing.Point(157, 45);
+            this.txbxidmateria.Location = new System.Drawing.Point(209, 55);
+            this.txbxidmateria.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txbxidmateria.Name = "txbxidmateria";
-            this.txbxidmateria.Size = new System.Drawing.Size(138, 22);
+            this.txbxidmateria.Size = new System.Drawing.Size(183, 26);
             this.txbxidmateria.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(30, 89);
+            this.label3.Location = new System.Drawing.Point(40, 110);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(188, 16);
+            this.label3.Size = new System.Drawing.Size(234, 20);
             this.label3.TabIndex = 1;
             this.label3.Text = "Horas impartidas a la semana";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(30, 45);
+            this.label2.Location = new System.Drawing.Point(40, 55);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 16);
+            this.label2.Size = new System.Drawing.Size(124, 20);
             this.label2.TabIndex = 0;
             this.label2.Text = "Id de la materia";
             // 
             // pictureBox2
             // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.pictureBox2.Image = global::Escuela2.Properties.Resources.logo_estado;
-            this.pictureBox2.Location = new System.Drawing.Point(725, 12);
+            this.pictureBox2.Location = new System.Drawing.Point(967, 15);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(118, 82);
+            this.pictureBox2.Size = new System.Drawing.Size(157, 101);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.pictureBox1.Image = global::Escuela2.Properties.Resources.logo_upt;
-            this.pictureBox1.Location = new System.Drawing.Point(38, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(51, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(118, 82);
+            this.pictureBox1.Size = new System.Drawing.Size(157, 101);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // VentanaMateria
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(144)))), ((int)(((byte)(46)))));
-            this.ClientSize = new System.Drawing.Size(880, 506);
+            this.ClientSize = new System.Drawing.Size(1173, 623);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "VentanaMateria";
             this.Text = "Materia";
             this.groupBox1.ResumeLayout(false);
