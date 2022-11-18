966c22ab89c01d40f3ec533a68712eda9f233a10
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2022-11-17 22:08:49 -0600

    Add files via upload

--- Jpasswordfield/src/jpasswordfield/Jpwf.java
+++ Jpasswordfield/src/jpasswordfield/Jpwf.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jpasswordfield;
+package JPasswordField;
 
 import javax.swing.JOptionPane;
 
