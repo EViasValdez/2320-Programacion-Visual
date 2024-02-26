373c6cbab7a83b6d61a1761fa1ae36b16131e479
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-02-26 14:00:53 -0600

    Add files via upload

--- ParesoImpares/ParesoImpares/Form1.Designer.cs
+++ ParesoImpares/ParesoImpares/Form1.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ListaOriginal = new System.Windows.Forms.ListBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Limpiar = new System.Windows.Forms.Button();
+            this.ListaNumeros = new System.Windows.Forms.ListView();
             this.PasarPares = new System.Windows.Forms.Button();
             this.PasarImpares = new System.Windows.Forms.Button();
-            this.ListaOriginal = new System.Windows.Forms.ListBox();
-            this.ListaNumeros = new System.Windows.Forms.ListView();
-            this.Limpiar = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -49,6 +49,24 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Lista original";
             // 
+            // ListaOriginal
+            // 
+            this.ListaOriginal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ListaOriginal.FormattingEnabled = true;
+            this.ListaOriginal.ItemHeight = 20;
+            this.ListaOriginal.Items.AddRange(new object[] {
+            "2",
+            "4",
+            "11",
+            "13",
+            "18",
+            "29",
+            "41"});
+            this.ListaOriginal.Location = new System.Drawing.Point(6, 21);
+            this.ListaOriginal.Name = "ListaOriginal";
+            this.ListaOriginal.Size = new System.Drawing.Size(234, 364);
+            this.ListaOriginal.TabIndex = 0;
+            // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.Limpiar);
@@ -59,6 +77,25 @@
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Numeros";
+            // 
+            // Limpiar
+            // 
+            this.Limpiar.Location = new System.Drawing.Point(82, 405);
+            this.Limpiar.Name = "Limpiar";
+            this.Limpiar.Size = new System.Drawing.Size(112, 35);
+            this.Limpiar.TabIndex = 3;
+            this.Limpiar.Text = "Limpiar";
+            this.Limpiar.UseVisualStyleBackColor = true;
+            // 
+            // ListaNumeros
+            // 
+            this.ListaNumeros.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ListaNumeros.HideSelection = false;
+            this.ListaNumeros.Location = new System.Drawing.Point(7, 22);
+            this.ListaNumeros.Name = "ListaNumeros";
+            this.ListaNumeros.Size = new System.Drawing.Size(249, 365);
+            this.ListaNumeros.TabIndex = 0;
+            this.ListaNumeros.UseCompatibleStateImageBehavior = false;
             // 
             // PasarPares
             // 
@@ -79,46 +116,6 @@
             this.PasarImpares.Text = "<< Impares";
             this.PasarImpares.UseVisualStyleBackColor = true;
             this.PasarImpares.Click += new System.EventHandler(this.PasarImpares_Click);
-            // 
-            // ListaOriginal
-            // 
-            this.ListaOriginal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ListaOriginal.FormattingEnabled = true;
-            this.ListaOriginal.ItemHeight = 20;
-            this.ListaOriginal.Items.AddRange(new object[] {
-            "2",
-            "4",
-            "10",
-            "12",
-            "14",
-            "18",
-            "20",
-            "24",
-            "28",
-            "40"});
-            this.ListaOriginal.Location = new System.Drawing.Point(6, 21);
-            this.ListaOriginal.Name = "ListaOriginal";
-            this.ListaOriginal.Size = new System.Drawing.Size(234, 364);
-            this.ListaOriginal.TabIndex = 0;
-            // 
-            // ListaNumeros
-            // 
-            this.ListaNumeros.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ListaNumeros.HideSelection = false;
-            this.ListaNumeros.Location = new System.Drawing.Point(7, 22);
-            this.ListaNumeros.Name = "ListaNumeros";
-            this.ListaNumeros.Size = new System.Drawing.Size(249, 365);
-            this.ListaNumeros.TabIndex = 0;
-            this.ListaNumeros.UseCompatibleStateImageBehavior = false;
-            // 
-            // Limpiar
-            // 
-            this.Limpiar.Location = new System.Drawing.Point(82, 405);
-            this.Limpiar.Name = "Limpiar";
-            this.Limpiar.Size = new System.Drawing.Size(112, 35);
-            this.Limpiar.TabIndex = 3;
-            this.Limpiar.Text = "Limpiar";
-            this.Limpiar.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
